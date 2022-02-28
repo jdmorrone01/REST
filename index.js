218622a8bb6ed7711a5f8e271badda3d0b4abd04
jdmorrone01 <74426727+jdmorrone01@users.noreply.github.com>
2022-02-27 22:26:00 -0500

    added answers for request first round

--- index.js
+++ index.js
@@ -94,15 +94,30 @@ function listAuthenicatedUserRepos()
 // 1. Write code for listBranches in a given repo under an owner. See list branches
 async function listBranches(owner,repo)
 {
-	let options = getDefaultOptions(`/`, "GET");
+	let options = getDefaultOptions("/repos/"+owner+"/"+repo, "GET");
 
 	// Send a http request to url and specify a callback that will be called upon its return.
 	return new Promise(function(resolve, reject)
 	{
 		request(options, function (error, response, body) {
 
+			if( error )
+			{
+				console.log( chalk.red( error ));
+				reject(error);
+				return; // Terminate execution.
+			}
+
 			// console.debug( options );
 			resolve( JSON.parse(body) );
+
+			for( var i = 0; i < obj.length; i++ )
+			{
+				var name = obj[i].name;
+				console.log( name );
+			}
+
+			resolve(obj);
 
 		});
 	});
@@ -111,9 +126,10 @@ async function listBranches(owner,repo)
 // 2. Write code to create a new repo
 async function createRepo(owner,repo)
 {
-	let options = getDefaultOptions("/", "POST");
+	let options = getDefaultOptions("/user/repos", "POST");
 
 	// Send a http request to url and specify a callback that will be called upon its return.
+	options.headers={"name": repo};
 	return new Promise(function(resolve, reject)
 	{
 		request(options, function (error, response, body) {
@@ -127,7 +143,8 @@ async function createRepo(owner,repo)
 // 3. Write code for creating an issue for an existing repo.
 async function createIssue(owner,repo, issueName, issueBody)
 {
-	let options = getDefaultOptions("/", "POST");
+	let options = getDefaultOptions("/repos/"+owner+"/"+repo+"/issues", "POST");
+	options.headers+={"owner":issueBody,"repo":repo,"title":issueName};
 
 	// Send a http request to url and specify a callback that will be called upon its return.
 	return new Promise(function(resolve, reject)
@@ -144,6 +161,7 @@ async function createIssue(owner,repo, issueName, issueBody)
 async function enableWikiSupport(owner,repo)
 {
 	let options = getDefaultOptions("/", "PATCH");
+	options.headers+={"has_wiki":true};
 
 	// Send a http request to url and specify a callback that will be called upon its return.
 	return new Promise(function(resolve, reject)
